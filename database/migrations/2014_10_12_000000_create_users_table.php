cd9782872c969ced5bdd38d3566f129e09ee21f6
Ayman <ayman9334@gmail.com>
2023-04-28 16:28:59 +0100

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('nom_utilisateur')->unique();
+            $table->string('logo')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('n_telephone');
+            $table->string('code_postal');
+            $table->string('ville');
+            $table->string('adress');
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+};
