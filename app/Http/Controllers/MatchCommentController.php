c8f3444bf547d13fea6c70e79102bd75ffe8b9db
Ayman El mimouni <ayman9334@gmail.com>
2023-04-29 23:37:04 +0100

    end database v0.1

---
+++ app/Http/Controllers/MatchCommentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Match_comment;
+use Illuminate\Http\Request;
+
+class MatchCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Match_comment $match_comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Match_comment $match_comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Match_comment $match_comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Match_comment $match_comment)
+    {
+        //
+    }
+}
